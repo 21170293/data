053f470ad5b5e63fd017eac783e990bac9842de5
Yash kumar shukla <39242081+21170293@users.noreply.github.com>
2020-07-03 14:55:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$data=$_GET['data'];
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <script></script>
+    </head>
+    <body>
+        <span id="snip"><?php echo $data; ?></span>
+    </body>
+</html>
